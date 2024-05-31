55763260d7732050dd68c4e3c64751b51622cb00
Badboy-404 <108590422+badboysabbir8@users.noreply.github.com>
2024-05-31 23:43:38 +0600

    Create settings.js

---
+++ controller/settings.js
@@ -0,0 +1,7 @@
+global.mongo_Db = "mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "arifurenijhum977@gmail.com";
+global.my_email_password = "qvrkdexzpxejcpji";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
